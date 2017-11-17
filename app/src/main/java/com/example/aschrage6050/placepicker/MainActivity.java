89872338d9a08467d72a85b9154bc898b57242e4
aschrage6050 <ashton2000@gmail.com>
2017-11-17 13:36:10 -0800

    this is the backup

--- app/src/main/java/com/example/aschrage6050/placepicker/MainActivity.java
+++ app/src/main/java/com/example/aschrage6050/placepicker/MainActivity.java
@@ -12,34 +12,32 @@ import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.webkit.WebView;
+
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
-import com.google.android.gms.common.GooglePlayServicesRepairableException;
-import com.google.android.gms.location.places.Place;
-import com.google.android.gms.location.places.ui.PlacePicker;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView placeNameText;
-    TextView placeAddressText;
-    WebView attributionText;
-    Button getPlaceButton;
+
+    test testThing = new test();
+    public double latitude;
+    public double longitude;
     private Button button;
+    public static final String TAG = MainActivity.class.getSimpleName();
     private TextView textView;
     private LocationManager locationManager;
     private LocationListener locationListener;
     /*https://www.youtube.com/watch?v=QNb_3QKSmMk*/
     private final static int MY_PERMISSION_FINE_LOCATION = 101;
     private final static int PLACE_PICKER_REQUEST = 1;
-    private final static LatLngBounds bounds = new LatLngBounds(new LatLng(51.5152192, -0.1321900), new LatLng(51.5166013, -0.1299262));
+
 
 
     @Override
@@ -51,9 +49,14 @@ public class MainActivity extends AppCompatActivity {
         textView = (TextView) findViewById(R.id.textView);
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         locationListener = new LocationListener() {
+
             @Override
             public void onLocationChanged(Location location) {
-                textView.append("\n "+location.getLatitude()+" "+location.getLongitude());
+                latitude = (location.getLatitude());
+                longitude = (location.getLongitude());
+                textView.append("\n " + location.getLatitude() + " " + location.getLongitude());
+                Log.d("GPS: ", location.getLatitude() + ", " + location.getLongitude());
+                Log.d(TAG, "" + latitude);
             }
 
             @Override
@@ -74,47 +77,29 @@ public class MainActivity extends AppCompatActivity {
         };
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET
             }, 10);
             return;
-        }else{
+        } else {
             configureButton();
         }
 
-        locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0, locationListener);
 
 
         requestPermission();
 
-        placeNameText = (TextView) findViewById(R.id.tvPlaceName);
-        placeAddressText = (TextView) findViewById(R.id.tvPlaceAddress);
-        attributionText = (WebView) findViewById(R.id.wvAttribution);
-        getPlaceButton = (Button) findViewById(R.id.btGetPlace);
-        getPlaceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
-                builder.setLatLngBounds(bounds);
-                try {
-                    Intent intent = builder.build(MainActivity.this);
-                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
-                } catch (GooglePlayServicesRepairableException e) {
-                    e.printStackTrace();
-                } catch (GooglePlayServicesNotAvailableException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        });
     }
 
-    void configureButton(){
+
+
+    void configureButton() {
         // first check for permissions
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
-                        ,10);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
             }
             return;
         }
@@ -124,9 +109,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //noinspection MissingPermission
                 locationManager.requestLocationUpdates("gps", 5000, 0, locationListener);
+
             }
         });
+
     }
+
     private void requestPermission() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -135,34 +123,20 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSION_FINE_LOCATION:
-                if (grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_LONG).show();
                     configureButton();
                     finish();
-                return;
+                    return;
                 }
                 break;
         }
     }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == PLACE_PICKER_REQUEST){
-            if (resultCode == RESULT_OK){
-                Place place = PlacePicker.getPlace(MainActivity.this, data);
-                placeNameText.setText(place.getName());
-                placeAddressText.setText(place.getAddress());
-                if (place.getAttributions() == null) {
-                    attributionText.loadData("no attribution", "text/html; charset=utf-8", "UFT-8");
-                } else {
-                    attributionText.loadData(place.getAttributions().toString(), "text/html; charset=utf-8", "UFT-8");
-                }
-            }
-        }
-    }
 }
+
+
